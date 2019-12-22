fa708294aae553c2e9ac33dbb2cab5ed6088c506
adiM3344 <adi.me123@gamil.com>
2019-12-22 19:15:59 +0200

    start ex3

--- Lexer.cpp
+++ Lexer.cpp
@@ -13,17 +13,46 @@ string* Lexer::lexer(string file_name) {
     while (file) {
         string line;
         getline(file, line);
-        bool operator_flag = false;
-        bool curly_flag = false;
         int place = 0;
         for (int i = 0; i < line.length(); i++) {
             if (line[line.length()-1] == '{') {
-                curly_flag = true;
+                while (line[i] != ' ') {
+                    i++;
+                }
+                tokens.push(line.substr(place, i - place));
+                i++;
+                place = i;
+                while (line[i] != '<' && line[i] != '>' && line[i] != '!' && line[i] != '=') {
+                    i++;
+                }
+                if (line[i-1] == ' ') {
+                    tokens.push(line.substr(place, i - 1 - place));
+                } else {
+                    tokens.push(line.substr(place, i - place));
+                }
+                place = i;
+                if (line[i + 1] == '=') {
+                    tokens.push(line.substr(place, 2));
+                    i++;
+                } else {
+                    tokens.push(line.substr(place, 1));
+                }
+                if (line[i + 1] == ' ') {
+                    i++;
+                }
+                place = i + 1;
+                i = line.length();
+                if (line[i - 2] == ' ') {
+                    tokens.push(line.substr(place, i - 2 - place));
+                } else {
+                    tokens.push(line.substr(place, i - 1 - place));
+                }
+                tokens.push("{");
             }
-            if (line[i] == '}') {
+            else if (line[i] == '}') {
                 tokens.push("}");
             }
-            if (line[i] == '(') {
+            else if (line[i] == '(') {
                 tokens.push(line.substr(place, i-place));
                 place = i + 1;
                 while (line[i] != ')') {
@@ -35,7 +64,7 @@ string* Lexer::lexer(string file_name) {
                 }
                 tokens.push(line.substr(place, i-place));
             }
-            if ((line[i] == '-' && line [i+1] == '>') || ((line[i] == '<' && line [i+1] == '-'))) {
+            else if ((line[i] == '-' && line [i+1] == '>') || ((line[i] == '<' && line [i+1] == '-'))) {
                 if (line[i-1] != ' ') {
                     tokens.push(line.substr(place, i-place));
                     place = i;
@@ -49,25 +78,22 @@ string* Lexer::lexer(string file_name) {
                     i++;
                 }
             }
-            if (line[i] == ' ') {
-                if ((line[i-1] == '=') || (line[i-1] == '<') || (line[i-1] == '>')) {
-                    operator_flag = true;
+            else if (line[i] == '=') {
+                if (line[i-1] != ' ') {
+                    tokens.push(line.substr(place, i-place));
+                    place = i;
                 }
+                tokens.push(line.substr(place, 1));
+                place = i + 1;
+                if (line[i+1] == ' ') {
+                    place++;
+                }
+                tokens.push(line.substr(place, line.length() - place));
+                i = line.length();
+            }
+            else if (line[i] == ' ') {
                 tokens.push(line.substr(place, i-place));
                 place = i + 1;
-            }
-            if (operator_flag) {
-                if (curly_flag == true) {
-                    i = line.length() - 2;
-                    tokens.push(line.substr(place, i - place));
-                    place = i + 1;
-                    i = line.length();
-                    tokens.push(line.substr(place, i - place));
-                }
-                else {
-                    i = line.length();
-                    tokens.push(line.substr(place, i - place));
-                }
             }
         }
     }
@@ -78,6 +104,5 @@ string* Lexer::lexer(string file_name) {
         tokens.pop();
         cout <<i<<": "<< arr[i]<<endl;
     }
-
     return arr;
 }
