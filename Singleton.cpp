506e453d2b082975b20721d2ea0824ee0b12b8e7
adiM3344 <adi.me123@gamil.com>
2019-12-23 14:28:17 +0200

    start ex3

---
+++ Singleton.cpp
@@ -0,0 +1,5 @@
+//
+// Created by adi on 12/23/19.
+//
+
+#include "Singleton.h"
---
+++ Singleton.h
@@ -0,0 +1,47 @@
+//
+// Created by adi on 12/23/19.
+//
+
+#ifndef EX3_SINGELTON_H
+#define EX3_SINGELTON_H
+
+#include <iostream>
+#include "Command.h"
+#include "ex1.h"
+#include <map>
+
+using namespace std;
+
+class Singleton {
+    static Singleton *instance;
+    map<string,Command*> commands_map;
+    string* lexer_arr;
+    map<string, Variable> symbol_table;
+
+public:
+    static Singleton *getInstance() {
+        if (!instance) {
+            instance = new Singleton;
+        }
+        return instance;
+    }
+    map<string, Command*>* getMap() {
+        return &this->commands_map;
+    }
+    map<string, Variable>* getSymbolTable() {
+        return &this->symbol_table;
+    }
+    string** getLexerArray() {
+        return &this->lexer_arr;
+    }
+    void setMap(map<string, Command*> map) {
+        this->commands_map = map;
+    }
+    void setSymbolTable(map<string, Variable> map) {
+        this->symbol_table = map;
+    }
+    void setLexerArray(string* arr) {
+        this->lexer_arr = arr;
+    }
+};
+#endif //EX3_SINGELTON_H
