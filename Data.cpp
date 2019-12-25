8d0afd9e8572ce0cd088fdcfd29cae6ba3aca00f
adiM3344 <adi.me123@gamil.com>
2019-12-25 16:06:03 +0200

    start ex3

--- Data.cpp
+++ Data.cpp
@@ -1,39 +1,44 @@
 #include "Data.h"
 
 void Data::InitMap() {
-//    Singleton* singleton = Singleton::getInstance();
-//    map<string,Command*> commands_map;
-//    singleton->setMap(commands_map);
-//    list<string> arr = *singleton->getLexerArray();
-//
-//    for(int i=0;i<arr->length(); i++){
-//        if(arr[i]=="openDataServer"){
-//            OpenServerCommand* c = new OpenServerCommand(arr[i+1]);
-//            commands_map.insert({arr[i],c});
-//            i++;
-//        }
-//        else if(arr[i]=="connectControlClient"){
-//            ConnectCommand* c = new ConnectCommand(arr[i+1],arr[i+2]);
-//            commands_map.insert({arr[i],c});
-//            i+=2;
-//        }
-//        else if(arr[i]=="var" && arr[i+2] == "="){
-//            DefineVarCommand* c = new DefineVarCommand(arr[i+1], arr[i+2]);
-//            commands_map.insert({arr[i],c});
-//            i+=2;
-//        }
-//        else if(arr[i]=="var"){
-//            DefineVarCommand* c = new DefineVarCommand(arr[i+1],arr[i+2],arr[i+4]);
-//            commands_map.insert({arr[i],c});
+    Singleton* singleton = Singleton::getInstance();
+    map<string,Command*> commands_map;
+    singleton->setMap(commands_map);
+    list<string>* l = singleton->getLexerArray();
+    list<string>::iterator it = l->begin();
+    string arr[l->size()];
+    for (int i = 0; i < l->size(); i++) {
+        arr[i] = *it;
+        it++;
+    }
+    for(int i=0;i<arr->size(); i++){
+        if(arr[i]=="openDataServer"){
+            OpenServerCommand* c = new OpenServerCommand(arr[i+1]);
+            commands_map.insert({arr[i],c});
+            i++;
+        }
+        else if(arr[i]=="connectControlClient"){
+            ConnectCommand* c = new ConnectCommand(arr[i+1],arr[i+2]);
+            commands_map.insert({arr[i],c});
+            i+=2;
+        }
+        else if(arr[i]=="var" && arr[i+2] == "="){
+            DefineVarCommand* c = new DefineVarCommand(arr[i+1], arr[i+2]);
+            commands_map.insert({arr[i],c});
+            i+=2;
+        }
+        else if(arr[i]=="var"){
+            DefineVarCommand* c = new DefineVarCommand(arr[i+1],arr[i+2],arr[i+4]);
+            commands_map.insert({arr[i],c});
+            i+=4;
+//        }  else if(arr[i]=="while"){
+//            WhileCommand* c = new WhileCommand(arr[i+1],arr[i+2],arr[i+4]);
+//            this->commands_map.insert({arr[i],c});
 //            i+=4;
-////        }  else if(arr[i]=="while"){
-////            WhileCommand* c = new WhileCommand(arr[i+1],arr[i+2],arr[i+4]);
-////            this->commands_map.insert({arr[i],c});
-////            i+=4;
-////        } else if(arr[i]=="if"){
-////            IfCommand* c = new IfCommand(arr[i+1],arr[i+2],arr[i+4]);
-////            this->commands_map.insert({arr[i],c});
-////            i+=4;
-//       }
-//    }
+//        } else if(arr[i]=="if"){
+//            IfCommand* c = new IfCommand(arr[i+1],arr[i+2],arr[i+4]);
+//            this->commands_map.insert({arr[i],c});
+//            i+=4;
+       }
+    }
 }
